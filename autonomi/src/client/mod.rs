RolandSherwin/stableset_net

fix(bootstrap): simplify the cache to be a fifo

autonomi/src/client/mod.rs
@@ -208,7 +208,7 @@ impl Client {
             ..Default::default()
         };
 
-        let peers = match peers_args.get_addrs(None, None).await {
+        let peers = match peers_args.get_bootstrap_addr(None, None).await {
             Ok(peers) => peers,
             Err(e) => return Err(e.into()),
         };

maidsafe/autonomi

autonomi/src/client/mod.rs
@@ -204,7 +204,7 @@ impl Client {
             local: config.local,
             ..Default::default()
         };
-        let initial_peers = match peers_args.get_addrs(None, None).await {
+        let initial_peers = match peers_args.get_bootstrap_addr(None, None).await {
             Ok(peers) => peers,
             Err(e) => return Err(e.into()),
         };