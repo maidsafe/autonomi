8b48762001b2b45b9e0f8e59b41546a2f5b7a92e
Roland Sherwin <RolandSherwin@protonmail.com>
2025-03-07 21:42:57 +0530

    chore(network): cleanup redundant network builder args

--- ant-networking/src/network_builder.rs
+++ ant-networking/src/network_builder.rs
@@ -247,8 +247,7 @@ impl NetworkBuilder {
 
         let listen_addr = self.listen_addr;
 
-        let (network, events_receiver, mut swarm_driver) =
-            self.build(kad_cfg, store_cfg, ProtocolSupport::Full);
+        let (network, events_receiver, mut swarm_driver) = self.build(kad_cfg, store_cfg);
 
         // Listen on the provided address
         let listen_socket_addr = listen_addr.ok_or(NetworkError::ListenAddressNotProvided)?;
@@ -269,7 +268,6 @@ impl NetworkBuilder {
         self,
         kad_cfg: kad::Config,
         record_store_cfg: NodeRecordStoreConfig,
-        req_res_protocol: ProtocolSupport,
     ) -> (Network, mpsc::Receiver<NetworkEvent>, SwarmDriver) {
         let identify_protocol_str = IDENTIFY_PROTOCOL_STR
             .read()
@@ -292,20 +290,20 @@ impl NetworkBuilder {
 
         // ==== Transport ====
         #[cfg(feature = "open-metrics")]
-        let main_transport = transport::build_transport(&self.keypair, &mut metrics_registries);
+        let transport = transport::build_transport(&self.keypair, &mut metrics_registries);
         #[cfg(not(feature = "open-metrics"))]
-        let main_transport = transport::build_transport(&self.keypair);
+        let transport = transport::build_transport(&self.keypair);
         let transport = if !self.local {
             debug!("Preventing non-global dials");
             // Wrap upper in a transport that prevents dialing local addresses.
-            libp2p::core::transport::global_only::Transport::new(main_transport).boxed()
+            libp2p::core::transport::global_only::Transport::new(transport).boxed()
         } else {
-            main_transport
+            transport
         };
 
-        let (relay_transport, relay_behaviour) =
+        let (relay_transport, relay_client) =
             libp2p::relay::client::new(self.keypair.public().to_peer_id());
-        let relay_transport = relay_transport
+        let transport = relay_transport
             .upgrade(libp2p::core::upgrade::Version::V1Lazy)
             .authenticate(
                 libp2p::noise::Config::new(&self.keypair)
@@ -314,7 +312,7 @@ impl NetworkBuilder {
             .multiplex(libp2p::yamux::Config::default())
             .or_transport(transport);
 
-        let transport = relay_transport
+        let transport = transport
             .map(|either_output, _| match either_output {
                 Either::Left((peer_id, muxer)) => (peer_id, StreamMuxerBox::new(muxer)),
                 Either::Right((peer_id, muxer)) => (peer_id, StreamMuxerBox::new(muxer)),
@@ -362,7 +360,7 @@ impl NetworkBuilder {
                 [(
                     StreamProtocol::try_from_owned(req_res_version_str)
                         .expect("StreamProtocol should start with a /"),
-                    req_res_protocol,
+                    ProtocolSupport::Full,
                 )],
                 cfg,
             )
@@ -436,7 +434,7 @@ impl NetworkBuilder {
             blocklist: libp2p::allow_block_list::Behaviour::default(),
             // `Relay client Behaviour` is enabled for all nodes. This is required for normal nodes to connect to relay
             // clients.
-            relay_client: relay_behaviour,
+            relay_client,
             relay_server,
             upnp,
             request_response,
