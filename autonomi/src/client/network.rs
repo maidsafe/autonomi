9440964f3b582e0a4b69e9510c715b379914da43
Warm Beer <mickvd99@gmail.com>
2025-03-18 17:43:53 +0100

    refactor(autonomi): move `get_closest_to_address` method to `network` mod from `quote`

--- autonomi/src/client/network.rs
+++ autonomi/src/client/network.rs
@@ -1,10 +1,23 @@
 use crate::Client;
 use ant_networking::version::Version;
+use ant_networking::{Addresses, NetworkError};
+use ant_protocol::NetworkAddress;
 use libp2p::PeerId;
 
 impl Client {
     /// Request the node version of a peer on the network.
     pub async fn get_node_version(&self, peer_id: PeerId) -> Result<Version, String> {
         self.network.get_node_version(peer_id).await
+    }
+
+    /// Retrieve the closest peers to the given network address.
+    /// This function queries the network to find all peers in the close group nearest to the provided network address.
+    pub async fn get_closest_to_address(
+        &self,
+        network_address: &NetworkAddress,
+    ) -> Result<Vec<(PeerId, Addresses)>, NetworkError> {
+        self.network
+            .client_get_all_close_peers_in_range_or_close_group(network_address)
+            .await
     }
 }
