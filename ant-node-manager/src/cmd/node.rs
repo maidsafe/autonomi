RolandSherwin/stableset_net

fix(bootstrap): simplify the cache to be a fifo

ant-node-manager/src/cmd/node.rs
@@ -109,7 +109,9 @@ pub async fn add(
 
     debug!("Parsing peers from PeersArgs");
 
-    peers_args.addrs.extend(PeersArgs::read_addr_from_env());
+    peers_args
+        .addrs
+        .extend(PeersArgs::read_bootstrap_addr_from_env());
     peers_args.bootstrap_cache_dir = bootstrap_cache_dir;
 
     let options = AddNodeServiceOptions {

maidsafe/autonomi

ant-node-manager/src/cmd/node.rs
@@ -111,7 +111,7 @@ pub async fn add(
 
     init_peers_config
         .addrs
-        .extend(InitialPeersConfig::read_addr_from_env());
+        .extend(InitialPeersConfig::read_bootstrap_addr_from_env());
     init_peers_config.bootstrap_cache_dir = bootstrap_cache_dir;
 
     let options = AddNodeServiceOptions {