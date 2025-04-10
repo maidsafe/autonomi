b9b355c8c4763cb58d3a0400f0064736a982800f
Roland Sherwin <RolandSherwin@protonmail.com>
2025-04-10 11:33:44 +0530

    fix: obtain addrs from connection info rather than identify

--- ant-node/src/networking/driver/event/identify.rs
+++ ant-node/src/networking/driver/event/identify.rs
@@ -376,6 +376,7 @@ impl SwarmDriver {
                 warn!("Peer {peer_id:?} is not part of the RT. Cannot update addresses.");
                 return;
             };
+
             let existing_addrs = entry
                 .node
                 .value
