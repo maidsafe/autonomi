b9940802c9c79d7642c7568f0595a8e4b1c760b5
Roland Sherwin <RolandSherwin@protonmail.com>
2025-04-23 12:40:36 +0530

    feat(network): rename nat detection to reachability check

--- ant-node/src/networking/mod.rs
+++ ant-node/src/networking/mod.rs
@@ -18,8 +18,8 @@ mod interface;
 mod log_markers;
 #[cfg(feature = "open-metrics")]
 mod metrics;
-mod nat_detection;
 mod network;
+mod reachability_check;
 mod record_store;
 mod relay_manager;
 mod replication_fetcher;
@@ -27,7 +27,7 @@ mod transport;
 
 // re-export arch dependent deps for use in the crate, or above
 pub use self::interface::SwarmLocalState;
-pub use self::nat_detection::ReachabilityStatus;
+pub use self::reachability_check::ReachabilityStatus;
 pub(crate) use self::{
     error::NetworkError,
     interface::{NetworkEvent, NodeIssue},
