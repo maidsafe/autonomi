76aad430059d10e4b105a9b22b37bf4b84aa17dd
Roland Sherwin <RolandSherwin@protonmail.com>
2025-04-23 12:40:36 +0530

    feat(network): rename nat detection to reachability check

--- ant-networking/src/lib.rs
+++ ant-networking/src/lib.rs
@@ -24,9 +24,9 @@ mod external_address;
 mod log_markers;
 #[cfg(feature = "open-metrics")]
 mod metrics;
-mod nat_detection;
 mod network_builder;
 mod network_discovery;
+mod reachability_check;
 mod record_store;
 mod relay_manager;
 mod replication_fetcher;
@@ -42,8 +42,8 @@ pub use self::{
     driver::SwarmDriver,
     error::NetworkError,
     event::{MsgResponder, NetworkEvent},
-    nat_detection::ReachabilityStatus,
     network_builder::{NetworkBuilder, MAX_PACKET_SIZE},
+    reachability_check::ReachabilityStatus,
     record_store::NodeRecordStore,
 };
 #[cfg(feature = "open-metrics")]
