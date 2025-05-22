2aefdb7f230eaaabab22f13cb2ac8d2e27bd4f48
Roland Sherwin <RolandSherwin@protonmail.com>
2025-05-22 14:38:07 +0530

    feat(reachability_check): return more results from the underlying swarm

--- ant-node/src/networking/error.rs
+++ ant-node/src/networking/error.rs
@@ -36,8 +36,6 @@ pub enum ReachabilityCheckError {
     LocalAdapterShouldNotBeEmpty,
     #[error("External address not found")]
     ExternalAddrsShouldNotBeEmpty,
-    #[error("No more contacts to dial")]
-    NoMoreContacts,
 }
 
 /// Network Errors
