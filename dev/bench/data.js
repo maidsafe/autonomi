adb211383cec0a30a45851d70b03369942897598
github-action-benchmark <github@users.noreply.github.com>
2025-06-04 10:44:14 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for ccf84d370101d38bc56e00d1eef336620f5f83c1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749033853115,
+  "lastUpdate": 1749033854365,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -106,6 +106,47 @@ window.BENCHMARK_DATA = {
             "name": "Average memory usage w/ upload",
             "value": 15,
             "unit": "MB"
+          }
+        ]
+      }
+    ],
+    "swarm_driver long handlings": [
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "distinct": true,
+          "id": "ccf84d370101d38bc56e00d1eef336620f5f83c1",
+          "message": "fix(CI): got benchchart client launch as local",
+          "timestamp": "2025-06-04T18:24:51+08:00",
+          "tree_id": "ff1f1ebe89ca01d5fa58afad0697cdc404a61866",
+          "url": "https://github.com/maidsafe/autonomi/commit/ccf84d370101d38bc56e00d1eef336620f5f83c1"
+        },
+        "date": 1749033854011,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 4921,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 11919,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 2,
+            "unit": "ms"
           }
         ]
       }
