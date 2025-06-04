97c58e4aa1a207aebdc51fc228fa1b4e8285c508
github-action-benchmark <github@users.noreply.github.com>
2025-06-04 10:44:13 +0000

    add Client memory (customSmallerIsBetter) benchmark result for ccf84d370101d38bc56e00d1eef336620f5f83c1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749033851840,
+  "lastUpdate": 1749033853115,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -69,6 +69,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 85,
+            "unit": "MB"
+          }
+        ]
+      }
+    ],
+    "Client memory": [
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "distinct": true,
+          "id": "ccf84d370101d38bc56e00d1eef336620f5f83c1",
+          "message": "fix(CI): got benchchart client launch as local",
+          "timestamp": "2025-06-04T18:24:51+08:00",
+          "tree_id": "ff1f1ebe89ca01d5fa58afad0697cdc404a61866",
+          "url": "https://github.com/maidsafe/autonomi/commit/ccf84d370101d38bc56e00d1eef336620f5f83c1"
+        },
+        "date": 1749033852750,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 17,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 15,
             "unit": "MB"
           }
         ]
