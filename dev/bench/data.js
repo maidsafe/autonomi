3dec394a7f50b67ab233ddfd716cc99fb4bebe97
github-action-benchmark <github@users.noreply.github.com>
2025-06-05 15:36:35 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 1c8a2c2439404bfdd2c0011092e025bb9ce76382

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749137794575,
+  "lastUpdate": 1749137795839,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -948,6 +948,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 11195,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 2,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mickvd99@gmail.com",
+            "name": "Mick van Dijke",
+            "username": "mickvandijke"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1c8a2c2439404bfdd2c0011092e025bb9ce76382",
+          "message": "Merge pull request #2739 from mickvandijke/example-spawn-single-node\n\nchore: add spawn single node example",
+          "timestamp": "2025-06-05T14:33:07Z",
+          "tree_id": "f2200f6de6d00c507ab904118207df4887cfb39c",
+          "url": "https://github.com/maidsafe/autonomi/commit/1c8a2c2439404bfdd2c0011092e025bb9ce76382"
+        },
+        "date": 1749137795521,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 4606,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 10516,
             "unit": "ms"
           },
           {
