4b069751f1bf065746c280fc0fd10cdc069ffb38
Roland Sherwin <RolandSherwin@protonmail.com>
2025-06-19 02:38:50 +0530

    feat(logging): instrument spawned task with current span

--- ant-node/src/networking/driver/mod.rs
+++ ant-node/src/networking/driver/mod.rs
@@ -53,6 +53,7 @@ use std::time::Instant;
 use tokio::sync::{mpsc, oneshot, watch};
 use tokio::time::{interval, Duration, Interval};
 use tracing::warn;
+use tracing::Instrument;
 
 use super::interface::{LocalSwarmCmd, NetworkEvent, NetworkSwarmCmd};
 
@@ -368,10 +369,8 @@ impl SwarmDriver {
                     event
                 );
             }
-            if let Err(error) = event_sender.send(event).await {
-                error!("SwarmDriver failed to send event: {}", error);
-            }
-        });
+            .instrument(tracing::Span::current()),
+        );
     }
 
     /// Sends an event after pushing it off thread so as to be non-blocking
@@ -388,10 +387,8 @@ impl SwarmDriver {
                     event
                 );
             }
-            if let Err(error) = event_sender.send(event).await {
-                error!("SwarmDriver failed to send event: {}", error);
-            }
-        });
+            .instrument(tracing::Span::current()),
+        );
     }
 
     /// Get K closest peers to self, from our local RoutingTable.
