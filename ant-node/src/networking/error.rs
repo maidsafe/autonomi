862416881f227e8cc84aefe8d24ada4b6487b67d
Roland Sherwin <RolandSherwin@protonmail.com>
2025-07-03 18:29:05 +0530

    Revert "Revert "refactor: network and swarm initialization code""
    This reverts commit afd60cf2e5982bfa3ea786d44480199f037aa554.

--- ant-node/src/networking/error.rs
+++ ant-node/src/networking/error.rs
@@ -108,9 +108,6 @@ pub enum NetworkError {
     #[error("Could not get enough peers ({required}) to satisfy the request, found {found}")]
     NotEnoughPeers { found: usize, required: usize },
 
-    #[error("Node Listen Address was not provided during construction")]
-    ListenAddressNotProvided,
-
     #[cfg(feature = "open-metrics")]
     #[error("Network Metric error")]
     NetworkMetricError,
