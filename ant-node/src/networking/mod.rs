0e739c07ce735a49bf11fa94114cdf9df3448b14
Roland Sherwin <RolandSherwin@protonmail.com>
2025-07-03 18:29:05 +0530

    Revert "Revert "refactor: network and swarm initialization code""
    This reverts commit afd60cf2e5982bfa3ea786d44480199f037aa554.

--- ant-node/src/networking/mod.rs
+++ ant-node/src/networking/mod.rs
@@ -31,14 +31,12 @@ pub use self::reachability_check::ReachabilityStatus;
 pub(crate) use self::{
     error::NetworkError,
     interface::{NetworkEvent, NodeIssue, SwarmLocalState},
-    network::{Network, NetworkBuilder},
+    network::{init_reachability_check_swarm, Network, NetworkConfig},
     record_store::NodeRecordStore,
 };
 
 #[cfg(feature = "open-metrics")]
 pub(crate) use metrics::service::MetricsRegistries;
-
-use interface::{LocalSwarmCmd, NetworkSwarmCmd};
 
 use self::error::Result;
 use ant_protocol::{NetworkAddress, CLOSE_GROUP_SIZE};
@@ -48,7 +46,6 @@ use libp2p::{
     Multiaddr, PeerId,
 };
 use std::net::SocketAddr;
-use tokio::sync::mpsc::Sender;
 
 /// Sort the provided peers by their distance to the given `KBucketKey`.
 /// Return with the closest expected number of entries it has.
@@ -163,45 +160,6 @@ pub(crate) fn multiaddr_get_ip(addr: &Multiaddr) -> Option<IpAddr> {
         Protocol::Ip6(ip) => Some(IpAddr::V6(ip)),
         _ => None,
     })
-}
-
-pub(crate) fn send_local_swarm_cmd(swarm_cmd_sender: Sender<LocalSwarmCmd>, cmd: LocalSwarmCmd) {
-    let capacity = swarm_cmd_sender.capacity();
-
-    if capacity == 0 {
-        error!(
-            "SwarmCmd channel is full. Await capacity to send: {:?}",
-            cmd
-        );
-    }
-
-    // Spawn a task to send the SwarmCmd and keep this fn sync
-    let _handle = tokio::spawn(async move {
-        if let Err(error) = swarm_cmd_sender.send(cmd).await {
-            error!("Failed to send SwarmCmd: {}", error);
-        }
-    });
-}
-
-pub(crate) fn send_network_swarm_cmd(
-    swarm_cmd_sender: Sender<NetworkSwarmCmd>,
-    cmd: NetworkSwarmCmd,
-) {
-    let capacity = swarm_cmd_sender.capacity();
-
-    if capacity == 0 {
-        error!(
-            "SwarmCmd channel is full. Await capacity to send: {:?}",
-            cmd
-        );
-    }
-
-    // Spawn a task to send the SwarmCmd and keep this fn sync
-    let _handle = tokio::spawn(async move {
-        if let Err(error) = swarm_cmd_sender.send(cmd).await {
-            error!("Failed to send SwarmCmd: {}", error);
-        }
-    });
 }
 
 /// Get the `SocketAddr` from the `Multiaddr`
