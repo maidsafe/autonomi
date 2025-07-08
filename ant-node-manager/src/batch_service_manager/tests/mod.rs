81bdcf4e02730483f35c8322bf5bf7d84b23cb3e
Roland Sherwin <RolandSherwin@protonmail.com>
2025-07-08 23:11:08 +0530

    feat(antctl): impl tests for batch service manager

---
+++ ant-node-manager/src/batch_service_manager/tests/mod.rs
@@ -0,0 +1,16 @@
+// Copyright (C) 2025 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under The General Public License (GPL), version 3.
+// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. Please review the Licences for the specific language governing
+// permissions and limitations relating to use of the SAFE Network Software.
+
+mod config_retention_tests;
+mod helpers;
+mod remove_tests;
+mod start_tests;
+mod stop_tests;
+mod upgrade_tests;
+
+// Re-export helpers for use in all test modules
