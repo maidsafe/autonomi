b07c59b610ee93a8307aa94e653a9125a8f48d36
Roland Sherwin <RolandSherwin@protonmail.com>
2025-08-08 08:19:35 +0530

    feat(node): use custom upnp behaviour

--- ant-node/src/networking/metrics/upnp.rs
+++ ant-node/src/networking/metrics/upnp.rs
@@ -6,6 +6,7 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
+use crate::networking::driver::behaviour::upnp;
 use prometheus_client::encoding::{EncodeLabelSet, EncodeLabelValue};
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq, EncodeLabelSet)]
@@ -21,19 +22,19 @@ enum EventType {
     NonRoutableGateway,
 }
 
-impl From<&libp2p::upnp::Event> for EventType {
-    fn from(event: &libp2p::upnp::Event) -> Self {
+impl From<&upnp::behaviour::Event> for EventType {
+    fn from(event: &upnp::behaviour::Event) -> Self {
         match event {
-            libp2p::upnp::Event::NewExternalAddr { .. } => EventType::NewExternalAddr,
-            libp2p::upnp::Event::ExpiredExternalAddr { .. } => EventType::ExpiredExternalAddr,
-            libp2p::upnp::Event::GatewayNotFound => EventType::GatewayNotFound,
-            libp2p::upnp::Event::NonRoutableGateway => EventType::NonRoutableGateway,
+            upnp::behaviour::Event::NewExternalAddr { .. } => EventType::NewExternalAddr,
+            upnp::behaviour::Event::ExpiredExternalAddr { .. } => EventType::ExpiredExternalAddr,
+            upnp::behaviour::Event::GatewayNotFound => EventType::GatewayNotFound,
+            upnp::behaviour::Event::NonRoutableGateway => EventType::NonRoutableGateway,
         }
     }
 }
 
-impl super::Recorder<libp2p::upnp::Event> for super::NetworkMetricsRecorder {
-    fn record(&self, event: &libp2p::upnp::Event) {
+impl super::Recorder<upnp::behaviour::Event> for super::NetworkMetricsRecorder {
+    fn record(&self, event: &upnp::behaviour::Event) {
         let _ = self
             .upnp_events
             .get_or_create(&UpnpEventLabels {
