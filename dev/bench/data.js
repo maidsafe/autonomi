718eb53547c7277569557fce9d78bddc3d8ec52d
github-action-benchmark <github@users.noreply.github.com>
2025-08-08 14:06:41 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for b32ac621dd170ef681d4c68d7bf521931613ee67

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754661999530,
+  "lastUpdate": 1754662001068,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -1653,6 +1653,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 22200,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 2,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b32ac621dd170ef681d4c68d7bf521931613ee67",
+          "message": "Merge pull request #3154 from grumbach/uploads_using_streaming\n\nfeat(client): implement streaming downloads for file_download and fil…",
+          "timestamp": "2025-08-08T13:26:23Z",
+          "tree_id": "21fb6225af9aad10bff49c54883f3317b5379f84",
+          "url": "https://github.com/maidsafe/autonomi/commit/b32ac621dd170ef681d4c68d7bf521931613ee67"
+        },
+        "date": 1754662000707,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 11394,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 23592,
             "unit": "ms"
           },
           {
