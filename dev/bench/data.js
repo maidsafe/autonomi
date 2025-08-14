00a9088801f429591caf0447da799044de30c631
github-action-benchmark <github@users.noreply.github.com>
2025-08-14 20:41:13 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 39eb71ffd9b5b8938721e3a0a5f4146d22dda565

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755104848375,
+  "lastUpdate": 1755204073321,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -584,6 +584,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "ant files download",
             "value": 26.11752158680508,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "39eb71ffd9b5b8938721e3a0a5f4146d22dda565",
+          "message": "Merge pull request #3162 from maqi/streaming_upload_tryout\n\nusing stream_encryption during CI",
+          "timestamp": "2025-08-14T21:32:54+01:00",
+          "tree_id": "70aa478f412380102558356bb35b8922c175a1d4",
+          "url": "https://github.com/maidsafe/autonomi/commit/39eb71ffd9b5b8938721e3a0a5f4146d22dda565"
+        },
+        "date": 1755204065948,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 2.538807895377489,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 8.944721845830912,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 27.04702978029338,
             "unit": "MiB/s"
           }
         ]
