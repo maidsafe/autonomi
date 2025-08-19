38107054a605ca6f5d79aac9173d6593f2f9b2df
github-action-benchmark <github@users.noreply.github.com>
2025-08-19 13:19:48 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 3a38745fb78945477140db25305809fc0cd67188

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755204156705,
+  "lastUpdate": 1755609588595,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -623,6 +623,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "ant files download",
             "value": 27.04702978029338,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3a38745fb78945477140db25305809fc0cd67188",
+          "message": "Merge pull request #3164 from RolandSherwin/ci_race_fix\n\nfix(node): add and sync on the same thread",
+          "timestamp": "2025-08-19T12:18:28Z",
+          "tree_id": "be8a99ba7a83c9a524026960776abb59f806ca73",
+          "url": "https://github.com/maidsafe/autonomi/commit/3a38745fb78945477140db25305809fc0cd67188"
+        },
+        "date": 1755609581469,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 2.779791240802301,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 9.722584066958262,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 27.046393975555176,
             "unit": "MiB/s"
           }
         ]
