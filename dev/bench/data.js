42c7cafd481ca6e7ce58dfd76bfdc35fb9623b8d
github-action-benchmark <github@users.noreply.github.com>
2025-08-22 12:47:03 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for ca2da0658be359855938d70084bb0918fdd6dde5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755866820593,
+  "lastUpdate": 1755866823161,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -2640,6 +2640,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 25195,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 2,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ca2da0658be359855938d70084bb0918fdd6dde5",
+          "message": "Merge pull request #3167 from RolandSherwin/bootstrap_fix\n\nfix(bootstrap): dont overwrite self with peers from fs",
+          "timestamp": "2025-08-22T11:50:34Z",
+          "tree_id": "e2e7ac0ae20cd0e905b7d634b17d6588dfcff75e",
+          "url": "https://github.com/maidsafe/autonomi/commit/ca2da0658be359855938d70084bb0918fdd6dde5"
+        },
+        "date": 1755866822245,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 11480,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 24092,
             "unit": "ms"
           },
           {
