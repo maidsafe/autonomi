959f6632512e37f7fa56f7362ad891e59da6a1e8
github-action-benchmark <github@users.noreply.github.com>
2025-09-10 17:13:33 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 5777b8cfb5f970a185ac12dfa2b319c65cc99dea

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757524410527,
+  "lastUpdate": 1757524413113,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -6165,6 +6165,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 20646,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 2,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5777b8cfb5f970a185ac12dfa2b319c65cc99dea",
+          "message": "Merge pull request #3195 from maqi/pointer_CI_tests\n\ntest(CI): pointer CI tests",
+          "timestamp": "2025-09-10T16:13:12Z",
+          "tree_id": "27b7e0be0089271d6ff778fd06d1e0928ded57f7",
+          "url": "https://github.com/maidsafe/autonomi/commit/5777b8cfb5f970a185ac12dfa2b319c65cc99dea"
+        },
+        "date": 1757524412099,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 11433,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 24212,
             "unit": "ms"
           },
           {
