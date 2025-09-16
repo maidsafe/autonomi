9055c64c843db87bd9f14d13b4004cbe72ccd5e4
Roland Sherwin <RolandSherwin@protonmail.com>
2025-09-16 23:04:16 +0530

    fix(antctl): fix failing windows tests - remove custom panic hooks

--- node-launchpad/src/bin/tui/main.rs
+++ node-launchpad/src/bin/tui/main.rs
@@ -106,7 +106,6 @@ fn main() -> Result<()> {
             }
         }
 
-        initialize_panic_handler()?;
         let args = Cli::parse();
 
         if args.version {
@@ -152,58 +151,6 @@ fn main() -> Result<()> {
     info!("Shutting down runtime");
     rt.shutdown_timeout(Duration::from_millis(100));
 
-    Ok(())
-}
-
-pub fn initialize_panic_handler() -> Result<()> {
-    let (panic_hook, eyre_hook) = color_eyre::config::HookBuilder::default()
-        .panic_section(format!(
-            "This is a bug. Consider reporting it at {}",
-            env!("CARGO_PKG_REPOSITORY")
-        ))
-        .capture_span_trace_by_default(false)
-        .display_location_section(false)
-        .display_env_section(false)
-        .into_hooks();
-    eyre_hook.install()?;
-    std::panic::set_hook(Box::new(move |panic_info| {
-        if let Ok(mut t) = node_launchpad::tui::Tui::new()
-            && let Err(r) = t.exit()
-        {
-            error!("Unable to exit Terminal: {:?}", r);
-        }
-
-        #[cfg(not(debug_assertions))]
-        {
-            use human_panic::{Metadata, handle_dump, print_msg};
-            let meta = Metadata {
-                version: env!("CARGO_PKG_VERSION").into(),
-                name: env!("CARGO_PKG_NAME").into(),
-                authors: env!("CARGO_PKG_AUTHORS").replace(':', ", ").into(),
-                homepage: "https://autonomi.com/".into(),
-            };
-
-            let file_path = handle_dump(&meta, panic_info);
-            // prints human-panic message
-            print_msg(file_path, &meta)
-                .expect("human-panic: printing error message to console failed");
-            eprintln!("{}", panic_hook.panic_report(panic_info)); // prints color-eyre stack trace to stderr
-        }
-        let msg = format!("{}", panic_hook.panic_report(panic_info));
-        error!("Error: {}", strip_ansi_escapes::strip_str(msg));
-
-        #[cfg(debug_assertions)]
-        {
-            // Better Panic stacktrace that is only enabled when debugging.
-            better_panic::Settings::auto()
-                .most_recent_first(false)
-                .lineno_suffix(true)
-                .verbosity(better_panic::Verbosity::Full)
-                .create_panic_handler()(panic_info);
-        }
-
-        std::process::exit(libc::EXIT_FAILURE);
-    }));
     Ok(())
 }
 
