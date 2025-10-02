97353bd4170b5a4402731001d2dabc5d68686006
github-action-benchmark <github@users.noreply.github.com>
2025-10-02 21:07:14 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 8ae89414911d9ca9007df1d0ffd33a24576c224e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759420753666,
+  "lastUpdate": 1759439234818,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -2456,6 +2456,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "ant files download",
             "value": 22.03259599996107,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8ae89414911d9ca9007df1d0ffd33a24576c224e",
+          "message": "Merge pull request #3229 from jacderida/chore-remove_nightly_workflows\n\nchore: remove various nightly workflows",
+          "timestamp": "2025-10-02T21:51:41+01:00",
+          "tree_id": "0a5b9dc89a5345c509cb689534687d0900c6c71a",
+          "url": "https://github.com/maidsafe/autonomi/commit/8ae89414911d9ca9007df1d0ffd33a24576c224e"
+        },
+        "date": 1759439227574,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 2.185158001463547,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 9.182274246001208,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 21.955635899803365,
             "unit": "MiB/s"
           }
         ]
