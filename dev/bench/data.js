b8b06c764539438f6b5f7bed16a96b633391ba5f
github-action-benchmark <github@users.noreply.github.com>
2025-10-02 14:57:40 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 7980385882dbb2f20b92412a364e8a0c51036121

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759417058440,
+  "lastUpdate": 1759417060177,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -6293,6 +6293,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 15,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7980385882dbb2f20b92412a364e8a0c51036121",
+          "message": "Merge pull request #3182 from mickvandijke/emission-service-cln-rebased\n\nfeat(autonomi): add `get_node_version` pub fn",
+          "timestamp": "2025-10-02T13:57:46Z",
+          "tree_id": "57d349243860e12211632d4f31ddeab8fb50b898",
+          "url": "https://github.com/maidsafe/autonomi/commit/7980385882dbb2f20b92412a364e8a0c51036121"
+        },
+        "date": 1759417059605,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 20,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 14,
             "unit": "MB"
           }
         ]
