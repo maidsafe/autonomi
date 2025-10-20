1bace82c03e0a7779e70e2e381c70f02e12d8dea
github-action-benchmark <github@users.noreply.github.com>
2025-10-20 12:24:06 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 68eb45d2b8e0a8bef6588f6330a8cda92f411b6b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760963044718,
+  "lastUpdate": 1760963046427,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -7313,6 +7313,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 14,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "68eb45d2b8e0a8bef6588f6330a8cda92f411b6b",
+          "message": "Merge pull request #3237 from maidsafe/dependabot/github_actions/astral-sh/setup-uv-7\n\nchore(deps): bump astral-sh/setup-uv from 6 to 7",
+          "timestamp": "2025-10-20T11:00:45Z",
+          "tree_id": "82f98b03ccfb3be26f274fada34d6a0b684eed17",
+          "url": "https://github.com/maidsafe/autonomi/commit/68eb45d2b8e0a8bef6588f6330a8cda92f411b6b"
+        },
+        "date": 1760963045869,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 15,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 13,
             "unit": "MB"
           }
         ]
