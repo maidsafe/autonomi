c048e07afdaa516aba96a07bb2cdb60005c7f91d
github-action-benchmark <github@users.noreply.github.com>
2025-10-27 18:14:16 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 57da1110b80777120e6eac7b926a7191fdca55a2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761580236655,
+  "lastUpdate": 1761588856536,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -3041,6 +3041,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "ant files download",
             "value": 7.032143759945233,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "57da1110b80777120e6eac7b926a7191fdca55a2",
+          "message": "Merge pull request #3274 from maidsafe/dependabot/github_actions/actions/upload-artifact-5\n\nchore(deps): bump actions/upload-artifact from 4 to 5",
+          "timestamp": "2025-10-27T17:08:32Z",
+          "tree_id": "a886fa23498b8f10ee73f9dd822d2082f8f4c735",
+          "url": "https://github.com/maidsafe/autonomi/commit/57da1110b80777120e6eac7b926a7191fdca55a2"
+        },
+        "date": 1761588848112,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 1.1223569480285391,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 6.348221420637628,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 7.0304069527947375,
             "unit": "MiB/s"
           }
         ]
