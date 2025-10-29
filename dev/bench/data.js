c1110e7426ae98c4d76a2c9c2643b271142d1571
github-action-benchmark <github@users.noreply.github.com>
2025-10-29 18:17:07 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 9f5582b69c9434884a3fa52b452b9a8481c9dd82

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761749619748,
+  "lastUpdate": 1761761827465,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -3197,6 +3197,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "ant files download",
             "value": 7.0612490736508935,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9f5582b69c9434884a3fa52b452b9a8481c9dd82",
+          "message": "Merge pull request #3278 from RolandSherwin/analyze_recursive\n\nfeat(ant): recursively analyze addresses",
+          "timestamp": "2025-10-29T17:06:35Z",
+          "tree_id": "8525e273b16aad7a19e44292db09e804d1f30a08",
+          "url": "https://github.com/maidsafe/autonomi/commit/9f5582b69c9434884a3fa52b452b9a8481c9dd82"
+        },
+        "date": 1761761820314,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 1.1274057321399453,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 6.210641835937303,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 7.049387632787648,
             "unit": "MiB/s"
           }
         ]
