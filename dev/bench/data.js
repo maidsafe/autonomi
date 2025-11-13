cc99196333cb9abadfaf276a945ea8fc4812fb46
github-action-benchmark <github@users.noreply.github.com>
2025-11-13 15:12:33 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 66ec46ce23645a5e6c76f0005044de2a3b848297

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763046669696,
+  "lastUpdate": 1763046753049,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -6160,6 +6160,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 185,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "66ec46ce23645a5e6c76f0005044de2a3b848297",
+          "message": "Merge pull request #3299 from maqi/blind_scan\n\nfurther storage tracking facilities from the analyze tool",
+          "timestamp": "2025-11-13T14:56:01Z",
+          "tree_id": "f5d149922289ce9d088f4182af801f233e18e152",
+          "url": "https://github.com/maidsafe/autonomi/commit/66ec46ce23645a5e6c76f0005044de2a3b848297"
+        },
+        "date": 1763046752357,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 206,
             "unit": "MB"
           }
         ]
