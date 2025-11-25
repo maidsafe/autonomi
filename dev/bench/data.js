2d3035a78e275c8bb9ef108cb496a0b029867f6c
github-action-benchmark <github@users.noreply.github.com>
2025-11-25 18:50:02 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 423c8ec13b95a4c458711401399bb22866b80edb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764096600323,
+  "lastUpdate": 1764096602817,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -10669,6 +10669,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/maidsafe/autonomi/commit/bc51c8f922d4de7c3667fb05bd5aa4a122eb1d30"
         },
         "date": 1764091928653,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 15,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 14,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "423c8ec13b95a4c458711401399bb22866b80edb",
+          "message": "Merge pull request #3311 from maidsafe/dependabot/npm_and_yarn/autonomi-nodejs/glob-10.5.0\n\nchore(deps): bump glob from 10.4.5 to 10.5.0 in /autonomi-nodejs",
+          "timestamp": "2025-11-25T17:42:26Z",
+          "tree_id": "c2bb235e28f07c383c84862824d8910bd28a91d2",
+          "url": "https://github.com/maidsafe/autonomi/commit/423c8ec13b95a4c458711401399bb22866b80edb"
+        },
+        "date": 1764096601955,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
