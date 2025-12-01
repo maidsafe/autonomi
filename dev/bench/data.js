8f1c57a82e9a425343ab2bd8fd29db10403af1e2
github-action-benchmark <github@users.noreply.github.com>
2025-12-01 16:30:10 +0000

    add Client memory (customSmallerIsBetter) benchmark result for d219d35d543ed02347a256da6302f948bdfa4f3f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764606607993,
+  "lastUpdate": 1764606610428,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -11179,6 +11179,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/maidsafe/autonomi/commit/97af816ddac226a91d9b204677408264bdbedfb8"
         },
         "date": 1764359223189,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 15,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 14,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d219d35d543ed02347a256da6302f948bdfa4f3f",
+          "message": "Merge pull request #3336 from maidsafe/dependabot/github_actions/actions/attest-build-provenance-3\n\nchore(deps): bump actions/attest-build-provenance from 1 to 3",
+          "timestamp": "2025-12-01T15:13:17Z",
+          "tree_id": "688a0bba7a5ff53f3f2166568f9ffc40aa4af9d8",
+          "url": "https://github.com/maidsafe/autonomi/commit/d219d35d543ed02347a256da6302f948bdfa4f3f"
+        },
+        "date": 1764606609490,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
