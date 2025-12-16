fbcdd687f46d802436c31d7f723f415a03c18d0b
github-action-benchmark <github@users.noreply.github.com>
2025-12-16 13:33:55 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 8a8fcbb907291483f486c317874df7ebdb7e717c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765892033905,
+  "lastUpdate": 1765892035250,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -12136,6 +12136,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/maidsafe/autonomi/commit/2e71058f8c19d64cb522daadfbe61b4e31a8bc14"
         },
         "date": 1765889673831,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 15,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 14,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "david.irvine@maidsafe.net",
+            "name": "David Irvine",
+            "username": "dirvine"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8a8fcbb907291483f486c317874df7ebdb7e717c",
+          "message": "Merge pull request #3322 from maqi/node_remove_closeup_bad_peer_from_RT\n\nfeat(node): carry out fetch check among closeup peers and block bad ones",
+          "timestamp": "2025-12-16T13:11:38Z",
+          "tree_id": "5fc2b624c64aac4ca9567ab22baab6cabcd5eec9",
+          "url": "https://github.com/maidsafe/autonomi/commit/8a8fcbb907291483f486c317874df7ebdb7e717c"
+        },
+        "date": 1765892034904,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
