e239560c1488c97890ca54c5093663463ed652ce
github-action-benchmark <github@users.noreply.github.com>
2026-01-09 21:45:22 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 1f7bbd8ecd90be0d2d6a4e1f71ce53f0e7819613

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767995120430,
+  "lastUpdate": 1767995122353,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -14550,6 +14550,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory usage w/ upload",
             "value": 15,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 14,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1f7bbd8ecd90be0d2d6a4e1f71ce53f0e7819613",
+          "message": "Merge pull request #3399 from maidsafe/rc-2025.12.3-hotfix\n\nchore(release): stable release 2025.12.3.2",
+          "timestamp": "2026-01-09T21:22:37Z",
+          "tree_id": "090a6b478d044487bc3115a3ce70f53d36fc52f8",
+          "url": "https://github.com/maidsafe/autonomi/commit/1f7bbd8ecd90be0d2d6a4e1f71ce53f0e7819613"
+        },
+        "date": 1767995121775,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 18,
             "unit": "MB"
           },
           {
