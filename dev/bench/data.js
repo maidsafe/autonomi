f94fc26b1cdf32e26443a8c80f09363e598dd06a
github-action-benchmark <github@users.noreply.github.com>
2026-01-13 13:50:16 +0000

    add Node memory (customSmallerIsBetter) benchmark result for af2299504095b42b95772c287d45dec385d18ae7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768312121392,
+  "lastUpdate": 1768312216325,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -10036,6 +10036,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 190,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "af2299504095b42b95772c287d45dec385d18ae7",
+          "message": "Merge pull request #3401 from maqi/cursor_rules\n\ncursor: rules for some skills",
+          "timestamp": "2026-01-13T12:32:58Z",
+          "tree_id": "9e44865dc8322f2376d66ebdfb740269e930de29",
+          "url": "https://github.com/maidsafe/autonomi/commit/af2299504095b42b95772c287d45dec385d18ae7"
+        },
+        "date": 1768312215516,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 168,
             "unit": "MB"
           }
         ]
