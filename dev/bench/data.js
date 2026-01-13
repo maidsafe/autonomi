d9e40eff8a47472e339b45d5af933fb286b7e9cf
github-action-benchmark <github@users.noreply.github.com>
2026-01-13 11:13:26 +0000

    add Client memory (customSmallerIsBetter) benchmark result for fc27d88abb9179fd54694ea5eb8680cfa8d986ad

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768302805536,
+  "lastUpdate": 1768302806873,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -14953,6 +14953,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/maidsafe/autonomi/commit/f838f350de18aee0a25379457f4e4fb17b5e7999"
         },
         "date": 1768263006308,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 15,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 14,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fc27d88abb9179fd54694ea5eb8680cfa8d986ad",
+          "message": "Merge pull request #3398 from vphongph/fix/ci-disk-space-cleanup\n\nfix(ci): add disk space cleanup step to prevent rust-cache failures",
+          "timestamp": "2026-01-13T09:52:39Z",
+          "tree_id": "200f4565a7029068ca2ee758a0ca2d5570e8c2eb",
+          "url": "https://github.com/maidsafe/autonomi/commit/fc27d88abb9179fd54694ea5eb8680cfa8d986ad"
+        },
+        "date": 1768302806519,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
