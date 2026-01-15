463e6a933e3b89a8161c3c3e6000796b3fcf3c83
github-action-benchmark <github@users.noreply.github.com>
2026-01-15 16:33:02 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 72b9fb091046e3859960972dfd64e197c7324c92

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768494780209,
+  "lastUpdate": 1768494782590,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -21398,6 +21398,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver average long handling time",
             "value": 2,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mickvd99@gmail.com",
+            "name": "Mick van Dijke",
+            "username": "mickvandijke"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "72b9fb091046e3859960972dfd64e197c7324c92",
+          "message": "Merge pull request #3394 from mickvandijke/feat-analytical-tools-get-quote\n\nfeat(ant): add `developer get-quote` command",
+          "timestamp": "2026-01-15T15:14:22Z",
+          "tree_id": "46e64ec2b8955568e55646a6a90d2a28bdfede97",
+          "url": "https://github.com/maidsafe/autonomi/commit/72b9fb091046e3859960972dfd64e197c7324c92"
+        },
+        "date": 1768494781742,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 10688,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 20846,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 1,
             "unit": "ms"
           }
         ]
