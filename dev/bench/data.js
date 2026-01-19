a72c8f8be6ad3f692ff72cccaad08eaac59f9c4e
github-action-benchmark <github@users.noreply.github.com>
2026-01-19 18:17:34 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 2255e28d8d14d011b6c20beb740d349ec73b29a0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768834602141,
+  "lastUpdate": 1768846654569,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -6005,6 +6005,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "ant files download",
             "value": 6.6440294182551405,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2255e28d8d14d011b6c20beb740d349ec73b29a0",
+          "message": "Merge pull request #3410 from mickvandijke/fix-node-req-res-timeout\n\nrefactor(ant-node): increase request timeout to 120 seconds",
+          "timestamp": "2026-01-19T17:00:21Z",
+          "tree_id": "5c164e21926eeb23e3246a48163f8386051e6196",
+          "url": "https://github.com/maidsafe/autonomi/commit/2255e28d8d14d011b6c20beb740d349ec73b29a0"
+        },
+        "date": 1768846647195,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 1.0227557955617217,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 6.012419356596101,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 6.674961331672856,
             "unit": "MiB/s"
           }
         ]
