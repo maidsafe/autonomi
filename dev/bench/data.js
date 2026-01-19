99d38394169bd17e0e265be9c8631936ddb0c9e9
github-action-benchmark <github@users.noreply.github.com>
2026-01-19 14:52:35 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 72da21671ee66d095b7d833aad922ef7ee7f1c5a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768834353675,
+  "lastUpdate": 1768834355910,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -21539,6 +21539,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver average long handling time",
             "value": 1,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "72da21671ee66d095b7d833aad922ef7ee7f1c5a",
+          "message": "Merge pull request #3413 from maidsafe/dependabot/npm_and_yarn/autonomi-nodejs/tar-7.5.3\n\nchore(deps): bump tar from 7.4.3 to 7.5.3 in /autonomi-nodejs",
+          "timestamp": "2026-01-19T13:34:35Z",
+          "tree_id": "21ba8f7c11a25847d36b13f0ecce5867c8516296",
+          "url": "https://github.com/maidsafe/autonomi/commit/72da21671ee66d095b7d833aad922ef7ee7f1c5a"
+        },
+        "date": 1768834355064,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 11126,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 23237,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 2,
             "unit": "ms"
           }
         ]
