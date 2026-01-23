8da2bc1e7cd661a51c9a6215e698dc383035c932
github-action-benchmark <github@users.noreply.github.com>
2026-01-23 04:31:39 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 9b89a372630c9fbab45d0e9c95521e61063b02d8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769102000557,
+  "lastUpdate": 1769142699852,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -6317,6 +6317,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "ant files download",
             "value": 6.931897877693995,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anselmega@gmail.com",
+            "name": "Anselme",
+            "username": "grumbach"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9b89a372630c9fbab45d0e9c95521e61063b02d8",
+          "message": "Merge pull request #3419 from grumbach/gas_info\n\nfeat: print information about gas costs",
+          "timestamp": "2026-01-23T03:12:54Z",
+          "tree_id": "9323149e835a1ee8886c8ef04ac752a448264396",
+          "url": "https://github.com/maidsafe/autonomi/commit/9b89a372630c9fbab45d0e9c95521e61063b02d8"
+        },
+        "date": 1769142691686,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 1.0243287632103855,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 5.417306924177326,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 6.986473736188522,
             "unit": "MiB/s"
           }
         ]
