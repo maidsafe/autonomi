0feade5d32ccf34f79d06fb37f44b510ed23700c
github-action-benchmark <github@users.noreply.github.com>
2026-01-27 12:13:41 +0000

    add Node memory (customSmallerIsBetter) benchmark result for bfc68572b34097ce34b1cfb24e31cb9328f97fbb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769515924332,
+  "lastUpdate": 1769516020985,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -11192,6 +11192,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 199,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anselmega@gmail.com",
+            "name": "Anselme",
+            "username": "grumbach"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bfc68572b34097ce34b1cfb24e31cb9328f97fbb",
+          "message": "Merge pull request #3422 from grumbach/cli_consistency_fixes\n\nCli consistency fixes",
+          "timestamp": "2026-01-27T11:01:57Z",
+          "tree_id": "4a696019a7fb984360a0da89ea10387827b04bfa",
+          "url": "https://github.com/maidsafe/autonomi/commit/bfc68572b34097ce34b1cfb24e31cb9328f97fbb"
+        },
+        "date": 1769516019890,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 229,
             "unit": "MB"
           }
         ]
