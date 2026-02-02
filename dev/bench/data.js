a487f7c5af6b83f48aa1afa0bcb0cc90069eff11
github-action-benchmark <github@users.noreply.github.com>
2026-02-02 12:35:32 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 7400ba1efe6bb834fec91823ab22f2ba1c605b9c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770035730891,
+  "lastUpdate": 1770035732244,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -25059,6 +25059,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 26819,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 2,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7400ba1efe6bb834fec91823ab22f2ba1c605b9c",
+          "message": "Merge pull request #3442 from maidsafe/dependabot/github_actions/digicert/ssm-code-signing-1.2.0\n\nchore(deps): bump digicert/ssm-code-signing from 1.1.1 to 1.2.0",
+          "timestamp": "2026-02-02T11:08:16Z",
+          "tree_id": "55a0c40103733763798517b7e418c9a1dfb379c9",
+          "url": "https://github.com/maidsafe/autonomi/commit/7400ba1efe6bb834fec91823ab22f2ba1c605b9c"
+        },
+        "date": 1770035731929,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 15152,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 33555,
             "unit": "ms"
           },
           {
