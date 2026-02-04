296c5d56333a0381d841c728aaa5629b199edbfe
github-action-benchmark <github@users.noreply.github.com>
2026-02-04 16:33:58 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 8f61750d692392441d3278e2d8f1c24c32aa72a0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770222836782,
+  "lastUpdate": 1770222838724,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -25910,6 +25910,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver average long handling time",
             "value": 1,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8f61750d692392441d3278e2d8f1c24c32aa72a0",
+          "message": "Merge pull request #3447 from jacderida/chore-set_minimum_node_version\n\nchore: update minimum node version",
+          "timestamp": "2026-02-04T15:04:56Z",
+          "tree_id": "1804b52ad6f9f5caf841b75201888cde4728ed61",
+          "url": "https://github.com/maidsafe/autonomi/commit/8f61750d692392441d3278e2d8f1c24c32aa72a0"
+        },
+        "date": 1770222838132,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 16261,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 33052,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 2,
             "unit": "ms"
           }
         ]
