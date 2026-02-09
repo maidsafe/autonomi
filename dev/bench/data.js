73907172ace26b6ad71595b4b898c4601e789841
github-action-benchmark <github@users.noreply.github.com>
2026-02-09 16:27:01 +0000

    add Node memory (customSmallerIsBetter) benchmark result for b27e4cefbc478939bc56ba9672cdeabc746a4892

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770654120094,
+  "lastUpdate": 1770654421271,
   "repoUrl": "https://github.com/maidsafe/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -12960,6 +12960,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 204,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b27e4cefbc478939bc56ba9672cdeabc746a4892",
+          "message": "Merge pull request #3454 from maqi/retry-failed_not_effective_with_merkle_upload\n\nnode(fix): honour --retry-failed for merkle uploads",
+          "timestamp": "2026-02-09T14:59:06Z",
+          "tree_id": "7b1fe53f3cf9b0e9f23f4c3d593170a3d1b721b7",
+          "url": "https://github.com/maidsafe/autonomi/commit/b27e4cefbc478939bc56ba9672cdeabc746a4892"
+        },
+        "date": 1770654419925,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 210,
             "unit": "MB"
           }
         ]
